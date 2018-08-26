b8aecc14622f8b7628c31bcaa2d5de0fc498cf5c
dt <derek.tingle@gmail.com>
2018-08-26 04:15:09 +0200

    checks interface and network

--- run.py
+++ run.py
@@ -3,10 +3,16 @@ import argparse
 import logging
 import json
 
-# from subprocess import Popen, PIPE
+from logging import handlers
+from subprocess import Popen, PIPE
 
 
 class Network():
+    def __init__(self):
+        self.args = self.parse_args()
+        self.logger = self.init_logging()
+        self.conf = self.load_conf()
+
     def init_logging(self):
         logger = logging.getLogger(__name__)
         log_format = "%(asctime)s -  %(message)s"
@@ -14,7 +20,10 @@ class Network():
 
         # write logs to log file
         if self.args.log_file:
-            handler = logging.FileHandler(self.args.log_file)
+            max_bytes = 1024 ** 2  # 1 mb
+            handler = handlers.RotatingFileHandler(
+                self.args.log_file, maxBytes=max_bytes
+            )
             logger.addHandler(handler)
         else:
             handler = logging.StreamHandler()
@@ -37,22 +46,32 @@ class Network():
         parser = argparse.ArgumentParser()
         parser.add_argument("-c", "--conf", required=True, help='verbose')
         parser.add_argument("-l", "--log_file", help="output log file")
-        parser.add_argument("-v", "--verbose", action='store_true',
-                            help='verbose')
+        parser.add_argument(
+            "-v", "--verbose", action='store_true', default=True,
+            help='verbose'
+        )
 
         return parser.parse_args()
 
-    def __init__(self):
-        self.args = self.parse_args()
-        self.logger = self.init_logging()
-        self.conf = self.load_conf()
+    def _device_is_on(self):
+        self.logger.info('checking if device is on')
+        cmd = ["networksetup", "-getairportpower", self.conf['device_name']]
+        proc = Popen(cmd, stdout=PIPE, stdin=PIPE)
+        out, err = proc.communicate()
+        return (proc.returncode == 0) and (out.decode('utf-8').strip().endswith("On"))
 
-        # TODO
-        self.logger.info(self.conf)
+    def _correct_network(self):
+        self.logger.info("checking if connected to network {}".format(
+            self.conf['network_name'])
+        )
+        cmd = ["networksetup", "-getairportnetwork", self.conf['device_name']]
+        proc = Popen(cmd, stdout=PIPE, stdin=PIPE)
+        out, err = proc.communicate()
+        return (proc.returncode == 0) and (out.decode('utf-8').strip().endswith(self.conf['network_name']))
 
-    def check_status(self):
-        pass
 
 
 if __name__ == '__main__':
     network = Network()
+    print(network._device_is_on())
+    print(network._correct_network())
